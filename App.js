63ba0a43ec35a700ce68c4b5377e1ffd5f876871
lonperman <lonperman@gmail.com>
2021-08-10 14:04:21 -0500

    Agregando el componente Provider y el plugin en el babel.config

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity} from 'react-native';
+import { TouchableOpacity} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Avatar} from 'react-native-paper';
+import { Avatar, Provider as PaperProvider} from 'react-native-paper';
 
 //Screen
 import HomeScreen from './screens/HomeScreen';
@@ -15,48 +15,50 @@ const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator >
-        <Stack.Screen name="HomeScreen"
-          component={HomeScreen}
-          options={({navigation}) => ({
-            title: 'Users',
-            headerStyle: {backgroundColor: '#EEEEEE'},
-            headerTitleStyle: { color: '#2b2b2b'},
-            headerRight: () => (
-             <TouchableOpacity  onPress={() => navigation.navigate('SearchUser')}>
-                <Avatar.Icon style={{backgroundColor:'#2b2b2b', marginRight: 20}}  size={40} icon='account-search' />
-             </TouchableOpacity>
-            ),
-            headerLeft: () => (
-              <Avatar.Icon style={{backgroundColor:'#2b2b2b', marginLeft: 10}}  size={40} icon='home' />
-            )   
-          })}
-        />
-        <Stack.Screen name="UserInfoScreen"
-          component={UserInfoScreen}
-          options={{
-            title: 'Details',
-            headerStyle: {backgroundColor: '#EEEEEE'},
-            headerTitleStyle: { color: '#2b2b2b'},
-            headerRight: () => (
-              <Avatar.Icon style={{backgroundColor:'#2b2b2b', marginRight: 20}}  size={40} icon='account-details'/>
-            ),   
-          }}
-        />
-        <Stack.Screen name="SearchUser"
-          component={SearchScreen}
-          options={{
-            title: 'Search',
-            headerStyle: {backgroundColor: '#EEEEEE'},
-            headerTitleStyle: { color: '#2b2b2b'},
-            headerRight: () => (
-              <Avatar.Icon style={{backgroundColor:'#2b2b2b', marginRight: 20}}  size={40} icon='account-details'/>
-            ),  
-          }}
-        />
+   <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator >
+          <Stack.Screen name="HomeScreen"
+            component={HomeScreen}
+            options={({navigation}) => ({
+              title: 'Users',
+              headerStyle: {backgroundColor: '#EEEEEE'},
+              headerTitleStyle: { color: '#2b2b2b'},
+              headerRight: () => (
+              <TouchableOpacity  onPress={() => navigation.navigate('SearchUser')}>
+                  <Avatar.Icon style={{backgroundColor:'#2b2b2b', marginRight: 20}}  size={40} icon='account-search' />
+              </TouchableOpacity>
+              ),
+              headerLeft: () => (
+                <Avatar.Icon style={{backgroundColor:'#2b2b2b', marginLeft: 10}}  size={40} icon='home' />
+              )   
+            })}
+          />
+          <Stack.Screen name="UserInfoScreen"
+            component={UserInfoScreen}
+            options={{
+              title: 'Details',
+              headerStyle: {backgroundColor: '#EEEEEE'},
+              headerTitleStyle: { color: '#2b2b2b'},
+              headerRight: () => (
+                <Avatar.Icon style={{backgroundColor:'#2b2b2b', marginRight: 20}}  size={40} icon='account-details'/>
+              ),   
+            }}
+          />
+          <Stack.Screen name="SearchUser"
+            component={SearchScreen}
+            options={{
+              title: 'Search',
+              headerStyle: {backgroundColor: '#EEEEEE'},
+              headerTitleStyle: { color: '#2b2b2b'},
+              headerRight: () => (
+                <Avatar.Icon style={{backgroundColor:'#2b2b2b', marginRight: 20}}  size={40} icon='account-details'/>
+              ),  
+            }}
+          />
       </Stack.Navigator>
     </NavigationContainer>
+   </PaperProvider>
   )
 }
 
